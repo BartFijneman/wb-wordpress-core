c9633731d8ed11eb9860ad1a433f49042cb303eb
Bart Fijneman <info@bartfijneman.com>
2022-01-04 11:41:33 +0100

    Updater

--- wb-wordpress-sso.php
+++ wb-wordpress-sso.php
@@ -7,7 +7,6 @@
  * Author: Webbedrijf.nl
  * Author URI: https://webbedrijf.nl
  * License: GPL2
- * GitHub Plugin URI: https://github.com/BartFijneman/wb-wordpress-sso
  */
 
 require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
@@ -18,9 +17,11 @@ new WbWordpressSso\Plugin();
 /*
  * Auto update
  */
-require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/BartFijneman/wb-wordpress-sso/plugin.json',
-    __FILE__, //Full path to the main plugin file or functions.php.
+    'https://github.com/BartFijneman/wb-wordpress-sso',
+    __FILE__,
     'wb-wordpress-sso'
 );
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
